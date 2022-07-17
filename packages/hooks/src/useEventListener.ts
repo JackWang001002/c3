038eb45a2e68f2c1c082e92ed81a0123e7698a48
jinbo.wang <che3vinci@gmail.com>
2022-07-17 11:28:34 +0800

    fix: useEventListenr

--- packages/hooks/src/useEventListener.ts
+++ packages/hooks/src/useEventListener.ts
@@ -2,13 +2,15 @@ import { addEventListener } from '@c3/dom';
 import { MutableRefObject, useEffect } from 'react';
 
 export const useEventListener = (
-  target: HTMLElement | MutableRefObject<HTMLElement> | Window | Document,
+  target: HTMLElement | Window | Document,
   eventName: string,
   cb: EventListener,
   option?: boolean | AddEventListenerOptions
 ) => {
   useEffect(() => {
-    const _target = 'current' in target ? target.current : target;
-    return addEventListener(_target, eventName, cb, option);
+    if (!target) {
+      return;
+    }
+    return addEventListener(target, eventName, cb, option);
   }, [cb, eventName, option, target]);
 };
