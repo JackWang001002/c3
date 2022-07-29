37e5cc26a1070723adbc796fd4c2e04ab82cdd45
jinbo.wang <che3vinci@gmail.com>
2022-07-29 22:25:33 +0800

    feat: useBus support callback with args

--- packages/hooks/src/useBus.ts
+++ packages/hooks/src/useBus.ts
@@ -1,6 +1,9 @@
 import { useCallback, useEffect } from 'react';
 import mitt, { Handler } from 'mitt';
+
 const emitter = mitt();
+//@ts-ignore
+window.emitter = emitter;
 
 export type BusEventNameType = `@event-bus/${string}`;
 
@@ -8,8 +11,10 @@ export const useBus = (event?: BusEventNameType, cb?: Handler) => {
   useEffect(() => {
     event && cb && emitter.on(event, cb);
   }, [cb, event]);
-  const emit = useCallback((event: BusEventNameType) => {
-    emitter.emit(event);
+
+  const emit = useCallback((event: BusEventNameType, ...args: any[]) => {
+    emitter.emit(event, ...args);
   }, []);
+
   return [emit, emitter] as const;
 };
