27ba039d415415c0f8ddbcb7f2187e546ecbfed6
jinbo.wang <che3vinci@gmail.com>
2022-10-13 10:15:34 +0800

    release

--- packages/hooks/src/useCallbackInEffect.ts
+++ packages/hooks/src/useCallbackInEffect.ts
@@ -1,12 +1,12 @@
 import { Fn } from '@c3/types';
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useLayoutEffect, useRef } from 'react';
 import { useSwitch } from './useSwitch';
 
 export const useNextTick = () => {
   const [runnable, on, off] = useSwitch(true);
   const ref = useRef<Fn>();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (runnable) {
       off();
       ref.current?.();
