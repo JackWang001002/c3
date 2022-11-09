58f3c6768522f810089e57ccb2e5f91894fbdc1b
jinbo.wang <che3vinci@gmail.com>
2022-11-09 15:20:57 +0800

    fix: placement

--- packages/utils/src/direction.ts
+++ packages/utils/src/direction.ts
@@ -16,17 +16,20 @@ export const getAntiDirection = (direction: Direction): Direction => {
 };
 
 export const anti: Record<Placement, Placement> = {
+  center: 'center',
   bottom: 'top',
   top: 'bottom',
   left: 'right',
+
   right: 'left',
-  center: 'center',
-  'bottom-left': 'top-right',
-  'bottom-right': 'top-left',
-  'top-left': 'bottom-right',
-  'top-right': 'bottom-left',
-  'left-top': 'right-bottom',
-  'left-bottom': 'right-top',
-  'right-top': 'left-bottom',
-  'right-bottom': 'left-top',
+  bottomLeft: 'topRight',
+  bottomRight: 'topLeft',
+  topLeft: 'bottomRight',
+
+  topRight: 'bottomLeft',
+  leftTop: 'rightBottom',
+  leftBottom: 'rightTop',
+
+  rightTop: 'leftBottom',
+  rightBottom: 'leftTop',
 };
