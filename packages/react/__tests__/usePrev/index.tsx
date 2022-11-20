aff1350b36420750c87d182a96098572ed7fdd79
jinbo.wang <che3vinci@gmail.com>
2022-11-20 11:04:53 +0800

    fix: useprev

---
+++ packages/react/__tests__/usePrev/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Case1 from './Case1';
+
+const root = ReactDOM.createRoot(document.getElementById('app')!);
+root.render(
+  <React.StrictMode>
+    <Case1 />
+  </React.StrictMode>
+);
