2334b6ab1f9815ed2ba8614d383d597af817cca6
jinbo.wang <che3vinci@gmail.com>
2022-11-29 17:28:04 +0800

    feat: useDelta

--- packages/react/src/hooks/useMouse.ts
+++ packages/react/src/hooks/useMouse.ts
@@ -5,6 +5,6 @@ type MouseListener<T extends HTMLElement | Document> = (e: React.MouseEvent<T>) 
 
 export const useMouse = <T extends HTMLElement | Document>(listener: MouseListener<T>) => {
   useEffect(() => {
-    return addEventListener(document, 'mousemove', listener);
+    return addEventListener(document, 'mousemove', listener as any);
   }, [listener]);
 };
