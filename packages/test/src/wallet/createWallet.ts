a790edc0b1aec24c3322325dad922c0f497edd2e
che3vinci <che3vinci@gmail.com>
2023-02-13 17:48:57 +0800

    chore: add release script

--- packages/test/src/wallet/createWallet.ts
+++ packages/test/src/wallet/createWallet.ts
@@ -1,12 +1,13 @@
-import { ethers } from 'ethers';
+// import { ethers } from 'ethers';
 
-export const createWallet = (
-  privateKey: string,
-  provider: string | ethers.providers.JsonRpcProvider
-) => {
-  const wallet = new ethers.Wallet(privateKey);
-  const _provider =
-    typeof provider === 'string' ? new ethers.providers.JsonRpcProvider(provider) : provider;
-  return wallet.connect(_provider);
-};
+// export const createWallet = (
+//   privateKey: string,
+//   provider: string | ethers.providers.JsonRpcProvider
+// ) => {
+//   const wallet = new ethers.Wallet(privateKey);
+//   const _provider =
+//     typeof provider === 'string' ? new ethers.providers.JsonRpcProvider(provider) : provider;
+//   return wallet.connect(_provider);
+// };
 
+export {};
