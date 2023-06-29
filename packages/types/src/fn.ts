3ce361e5afba87e1e4d23ef1d360dd1952849da5
tom <che3vincigmail.com>
2023-06-29 17:22:46 +0800

    chore: release

--- packages/types/src/fn.ts
+++ packages/types/src/fn.ts
@@ -1,4 +1,4 @@
-export type Fn = (...args: unknown[]) => unknown;
+export type Fn = (...args: any[]) => any;
 
 export type WithoutCallSignature<T extends Fn> = {
   [K in keyof T as Exclude<K, "">]: T[K];
