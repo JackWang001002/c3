b7c5bce73d41306727736e2cef3a0d21d6fdf27d
tom <che3vincigmail.com>
2023-11-22 21:51:21 +0800

    fix: toCamelCase

--- packages/utils/src/string/firstUpper.ts
+++ packages/utils/src/string/firstUpper.ts
@@ -1,3 +1,4 @@
-export const REG_FIRST_CHAR = /^./;
+export const firstCharReg = /^./;
 
-export const firstUpper = (s: string) => s.replace(REG_FIRST_CHAR, m => m.toUpperCase());
+export const firstUpper = (s: string) => s.replace(firstCharReg, m => m.toUpperCase());
+export const toCamelCase = (s: string) => s.replace(/[-_](.)/g, (_, m) => m.toUpperCase());
