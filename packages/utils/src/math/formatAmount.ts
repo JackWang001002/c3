cb99253a1827cda689e67e177ed06892a7e53a35
gunner6 <gunner6@163.com>
2023-12-15 18:09:44 +0800

    fix: update c3/utils

--- packages/utils/src/math/formatAmount.ts
+++ packages/utils/src/math/formatAmount.ts
@@ -1,13 +1,31 @@
 import BigNumber from "bignumber.js";
 BigNumber.config({ ROUNDING_MODE: BigNumber.ROUND_FLOOR });
 
-export const formatAmount = (
+export const RoundingMode = {
+  ROUND_UP: 0,
+  ROUND_DOWN: 1,
+  ROUND_CEIL: 2,
+  ROUND_FLOOR: 3,
+  ROUND_HALF_UP: 4,
+  ROUND_HALF_DOWN: 5,
+  ROUND_HALF_EVEN: 6,
+  ROUND_HALF_CEIL: 7,
+  ROUND_HALF_FLOOR: 8,
+  EUCLID: 9,
+};
+export function formatAmount(value: string, decimal: number): void;
+export function formatAmount(
   value: string,
   decimal: number,
   roundingMode: BigNumber.RoundingMode
-) => {
-  if (roundingMode) {
+): void;
+export function formatAmount(
+  value: string,
+  decimal: number,
+  roundingMode?: BigNumber.RoundingMode
+) {
+  if (roundingMode != undefined) {
     return new BigNumber(value).toFixed(decimal, roundingMode);
   }
   return new BigNumber(value).toFixed(decimal);
-};
+}
