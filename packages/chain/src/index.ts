78c97a7e6c699762fdaed159f59d90793f1ddb20
tom <che3vincigmail.com>
2024-01-11 20:20:45 +0800

    feat: c3

--- packages/chain/src/index.ts
+++ packages/chain/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./types";
 export * from "./chains";
 export * from "./rawChainData";
+export * from "./getValidRpc";
+export * from "./utils";
---
+++ packages/chain/src/log.ts
@@ -0,0 +1,2 @@
+import { cdbg } from "@c3/dbg";
+export const log = cdbg("@c3/chain →", "color:blue;background:white", "@c3/chain", "log");
