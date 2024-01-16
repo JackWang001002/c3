71b0696375235cae7bf203614b2bd7cbf3874ec8
tom <che3vincigmail.com>
2024-01-16 15:00:06 +0800

    chore: release process

--- scripts/cli.mjs
+++ scripts/cli.mjs
@@ -8,8 +8,8 @@ run({
     const { semver = "patch" } = option;
     await $`gh auth login --with-token < ~/.github-access-token-che3vinci `;
     await $`pnpm -r build`;
-    await $`git add .`;
-    await $`git commit -m "chore: release"`;
+    // await $`git add .`;
+    // await $`git commit -m "chore: release"`;
     await $`git push`;
     await $`lerna version ${semver} --conventional-commits --no-commit-hooks -y`;
     // await $`npm login`; //TODO:设置npmrc的access token
