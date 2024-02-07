dad458655f511ea1d4e78b4c7fcac7e37741f50d
gunner6 <gunner6@163.com>
2024-02-07 17:59:41 +0800

    chore: release

--- scripts/cli.mjs
+++ scripts/cli.mjs
@@ -10,9 +10,9 @@ run({
     await $`pnpm -r build`;
     await $`git add .`;
     await $`git commit -m "chore: release"`;
-    await $`git push --set-upstream origin main`;
+    // await $`git push --set-upstream origin main`;
     await $`lerna version ${semver} --conventional-commits --no-commit-hooks -y`;
     // await $`npm login`; //TODO:设置npmrc的access token
-    await $`pnpm -r publish ----report-summary`;
+    // await $`pnpm -r publish ----report-summary`;
   },
 });
