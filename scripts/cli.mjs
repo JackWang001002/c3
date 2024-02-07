7e9bb151e71b6e24bad02cf6114277e3e638339c
gunner6 <gunner6@163.com>
2024-02-07 17:03:57 +0800

    chore: release

--- scripts/cli.mjs
+++ scripts/cli.mjs
@@ -6,10 +6,10 @@ import { $ } from "zx";
 run({
   async release(option) {
     const { semver = "patch" } = option;
-    await $`gh auth login --with-token < ~/.github-access-token-che3vinci `;
+    // await $`gh auth login --with-token < ~/.github-access-token-che3vinci `;
     await $`pnpm -r build`;
-    // await $`git add .`;
-    // await $`git commit -m "chore: release"`;
+    await $`git add .`;
+    await $`git commit -m "chore: release"`;
     await $`git push`;
     await $`lerna version ${semver} --conventional-commits --no-commit-hooks -y`;
     // await $`npm login`; //TODO:设置npmrc的access token
